7ef61d97311da4bbeb291698e587433863ef7499
Emre Yavuzyiğit <emre.yavuz.842@gmail.com>
2016-03-03 06:17:09 +1200

    P

--- src/core/Core.php
+++ src/core/Core.php
@@ -1,1 +1,13 @@
+<?php
 
+namespace FeatureCore;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat;
+use pocketmine\Plugin;
+use pocketmine\Player;
+use pocketmine\level\Level;
+use pocketmine\Server;
+
+class Core extends PluginBase implements Listener{
